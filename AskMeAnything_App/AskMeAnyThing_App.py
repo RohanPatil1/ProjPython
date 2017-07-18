df907c79ef97a2ec14dc0175f22489f24e42175d
RohanPatil <prorohan8@gmail.com>
2017-07-18 20:23:47 +0530

    The AskMeAnything App
    My First Ever Python App

---
+++ AskMeAnything_App/AskMeAnyThing_App.py
@@ -0,0 +1,64 @@
+from Tkinter import *
+
+import wikipedia
+import wolframalpha
+
+import tkinter as tk,tkFont
+root=Tk()
+root.configure(background='#F50A44')
+root.geometry('423x423')
+root.title('AskMeAnyThing App')
+topframe=Frame(root,width=210, height=200,bg='#F50A44')
+topframe.pack()
+
+customFont = tkFont.Font(family="Gloria Hallelujah", size=16)
+customFont1 = tkFont.Font(family="Oswald", size=14)
+customFont2 = tkFont.Font(family="Consolas", size=10)
+customFont3 = tkFont.Font(family="Oswald", size=10)
+customFontR = tkFont.Font(family="Gloria Hallelujah", size=11)
+
+welcomeLabel=Label(topframe,text="Welcome To Our AskMeAnyThing App",fg='#d8ff4f',font=customFont,bg='#F50A44')
+
+welcomeLabel.pack(side=TOP)
+
+qlabel=Label(topframe,text="Enter Your Question : ",bg='#F50A44',font=customFont1,fg='white')
+
+qlabel.pack(side=LEFT,pady=90)
+E1=Entry(topframe,bd=3,bg='#01799e',width=36,fg='white',font=customFont2)
+
+E1.pack(side=LEFT,pady=90)
+
+
+def getQue():
+
+
+        question = E1.get()
+        ans = StringVar()
+        window = Toplevel(topframe)
+        window.configure(background='#F50A44')
+        l=Message(window,textvariable=ans)
+        l.pack()
+
+
+        try:
+            # wolframalpha
+            app_id = "KHUJLG-YUVT8K9V33"
+            client = wolframalpha.Client(app_id)
+            res = client.query(question)
+            answer1 = next(res.results).text
+            ans.set(answer1)
+
+
+        except:
+            # wikipedia
+            answer1 =wikipedia.summary(question)
+            ans.set(answer1)
+
+getButton=Button(topframe,text="Get Answer!",command=getQue,width=20,height=2,relief=RAISED,bg='#65fc5f',font=customFont3)
+getButton.place(relx=0.5, rely=1.0, anchor='s')
+
+lr=Label(root,text="- Developed By Rohan",bg='#F50A44',fg='#d8ff4f',font=customFontR)
+lr.place(relx=1.0, rely=1.0, anchor='se')
+root.mainloop()
+
+root.mainloop()
