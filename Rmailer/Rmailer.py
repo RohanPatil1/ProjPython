8a3a995db8f4972c35be223c9fa9362bc898dc78
RohanPatil1 <prorohan8@gmail.com>
2017-07-20 18:18:11 +0530

    Update Rmailer.py

--- Rmailer/Rmailer.py
+++ Rmailer/Rmailer.py
@@ -1,6 +1,14 @@
+
+#(Jul 2017)GUI BUILD IN PYTHON BY Rohan Patil
+#
+#----------------------------------------------Python_Imports-------------------------------------------------------------------#
+
 import smtplib
 import tkinter as tk,tkFont
 from tkinter import Text, Tk
+from Tkinter import *
+import tkMessageBox
+#----------------------------------------Function_To_Run_On_Button_Pressed------------------------------------------------------#
 
 def mainProccess():
 
@@ -10,14 +18,13 @@ def mainProccess():
     mail.login(eEntry.get(), le.get())
     mail.sendmail(eEntry.get(), rme.get(), yme.get())
     mail.close()
-from Tkinter import *
-import tkMessageBox
 
 
 
 
 
-# -------------------------TKINTER-----------------------------------------------------------#
+
+# ------------------------------------------------Roots_&_Frames----------------------------------------------------------------#
 
 root=Tk()
 root.title("Rmailer")
@@ -28,14 +35,15 @@ topframe.pack()
 
 bottomFrame=Frame()
 bottomFrame.pack(side=BOTTOM)
-
+# --------------------------------------------------Custom_Fonts----------------------------------------------------------------#
 customFont = tkFont.Font(family="Magneto", size=18)
 customFont1 = tkFont.Font(family="Oswald", size=14)
 customFont2 = tkFont.Font(family="Raleway", size=10)
-customFont3 = tkFont.Font(family="Oswald", size=10)
+
 customFontR = tkFont.Font(family="Gloria Hallelujah", size=11)
 customFontB=tkFont.Font(family="Century Gothic",size=17)
 
+# ------------------------------------------------TKINTER_BUILDS----------------------------------------------------------------#
 
 welcomeLabel=Label(topframe,text="Welcome To Our Rmailer App",fg='#01256d',bg='#00f4bb',font=customFont)
 welcomeLabel.pack(side=TOP)
@@ -59,17 +67,11 @@ lr.place(relx=0.26, rely=0.7, anchor='se')
 le=Entry(root,bd=4,width=30,font=customFont2,show="*",bg="#ffff6b")
 le.place(relx=0.835, rely=0.7, anchor='se')
 
-
-
-
-
 rm=Label(root,text="Receiver's Mail  :",bg='#00f4bb',fg='white',font=customFont1)
 rm.place(relx=0.28, rely=0.79, anchor='e')
 
 rme=Entry(root,bd=4,width=30,font=customFont2,bg="#ffff6b")
 rme.place(relx=0.84, rely=0.8, anchor='e')
-
-
 
 ym=Label(root,text="Your Message  :",bg='#00f4bb',fg='white',font=customFont1)
 ym.place(relx=0.25, rely=0.93, anchor='e')
@@ -78,4 +80,6 @@ yme=Entry(root,bd=4,width=30,font=customFont2,bg="#ffff6b")
 yme.place(relx=0.84, rely=0.93, anchor='e')
 
 root.mainloop()
+
+#--------------------------------------------------Email:prorohan8@gmail.com--------------------------------------------------#
 
