a5ef8ff2dabfdfdca566d07e7e29440a0ebaf949
RohanPatil1 <prorohan8@gmail.com>
2018-01-18 22:55:45 +0530

    Update TrapCaller.py

--- TrapCaller/TrapCaller.py
+++ TrapCaller/TrapCaller.py
@@ -1,4 +1,4 @@
-#(Jul 2017)GUI Build With Python Tkinter By Rohan Patil
+#(Jul 2017)
 #To Preview How It Works, Visit : https://www.youtube.com/watch?v=wA6plE3ukfk
 #---------------------------------------------IMPORTS-----------------------------------------------------------------------#
 import trunofficial
